45479ceb45e80305540b15e04898e792ef177e9d
sdustzxy <97588357+sdustzxy@users.noreply.github.com>
2024-09-09 20:25:45 +0800

    Update eca_atten.py

--- BFG/eca_atten.py
+++ BFG/eca_atten.py
@@ -1,52 +1,33 @@
 import torch
 from torch import nn
 import math
-# from torchstat import stat  # 查看网络参数
 
 
-# 定义ECANet的类
 class eca_block(nn.Module):
-    # 初始化, in_channel代表特征图的输入通道数, b和gama代表公式中的两个系数
     def __init__(self, in_channel, b=1, gama=2):
-        # 继承父类初始化
         super(eca_block, self).__init__()
 
-        # 根据输入通道数自适应调整卷积核大小
         kernel_size = int(abs((math.log(in_channel, 2) + b) / gama))
-        # 如果卷积核大小是奇数，就使用它
         if kernel_size % 2:
             kernel_size = kernel_size
-        # 如果卷积核大小是偶数，就把它变成奇数
         else:
             kernel_size = kernel_size
 
-        # 卷积时，为例保证卷积前后的size不变，需要0填充的数量
         padding = kernel_size // 2
 
-        # 全局平均池化，输出的特征图的宽高=1
         self.avg_pool = nn.AdaptiveAvgPool2d(output_size=1)
-        # 1D卷积，输入和输出通道数都=1，卷积核大小是自适应的
         self.conv = nn.Conv1d(in_channels=1, out_channels=1, kernel_size=kernel_size,
                               bias=False, padding=padding)
-        # sigmoid激活函数，权值归一化
         self.sigmoid = nn.Sigmoid()
 
-    # 前向传播
     def forward(self, inputs):
-        # 获得输入图像的shape
         b, c, h, w = inputs.shape
 
-        # 全局平均池化 [b,c,h,w]==>[b,c,1,1]
         x = self.avg_pool(inputs)
-        # 维度调整，变成序列形式 [b,c,1,1]==>[b,1,c]
         x = x.view([b, 1, c])
-        # 1D卷积 [b,1,c]==>[b,1,c]
         x = self.conv(x)
-        # 权值归一化
         x = self.sigmoid(x)
-        # 维度调整 [b,1,c]==>[b,c,1,1]
         x = x.view([b, c, 1, 1])
 
-        # 将输入特征图和通道权重相乘[b,c,h,w]*[b,c,1,1]==>[b,c,h,w]
         outputs = x * inputs
         return outputs
